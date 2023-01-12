d7b0c92d07527dbeade4395b88ce6db2b1366050
PaoloWD <paolo.lore.93@gmail.com>
2023-01-12 17:50:37 +0100

    complete join

--- join.sql
+++ join.sql
@@ -33,15 +33,21 @@ ORDER BY `students`.`surname`, `students`.`name`;
 
 -- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
 
+SELECT *
+FROM `degrees`
+INNER JOIN `courses` ON `degrees`.`id` = `courses`.`degree_id`
+INNER JOIN `course_teacher` ON `course_teacher`.`course_id` = `courses`.`id`
+INNER JOIN `teachers` ON `teachers`.`id `= `course_teacher`.`teacher_id`;
+
 
 -- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
-SELECT `teachers`.`surname` AS `cognome_docente`
+
+SELECT DISTINCT CONCAT(`teachers`.`name`, " ", `teachers`.`surname`) AS `formatted_name`
 FROM `teachers`
 INNER JOIN `course_teacher` ON `teachers`.`id` = `course_teacher`.`teacher_id`
 INNER JOIN `courses` ON `course_teacher`.`course_id` = `courses`.`id`
 INNER JOIN `degrees` ON `courses`.`degree_id` = `degrees`.`id`
 INNER JOIN `departments` ON `degrees`.`department_id` = `departments`.`id`
-WHERE `departments`.`name` = `Dipartimento di Matematica`
-GROUP BY `cognome_docente`;
+WHERE `departments`.`name` = "Dipartimento di Matematica";
 
 
